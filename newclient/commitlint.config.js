0444630b6348113ef962077996cdb06249593ba3
Kirill <planetastock@gmail.com>
2021-03-23 18:34:27 +0200

    change pre-commit

--- newclient/commitlint.config.js
+++ newclient/commitlint.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'type-enum': [2, 'always', ['build', 'ci', 'devop', 'chore', 'docs', 'feat', 'fix', 'refactor', 'revert', 'style', 'lint', 'test', 'translations']]
+        'type-enum': [2, 'always', ['build', 'ci', 'devop', 'develop', 'chore', 'docs', 'feat', 'fix', 'refactor', 'revert', 'style', 'lint', 'test', 'translations']]
     }
 }
