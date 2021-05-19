6e0bf1e65bd63b6d2c416e313503245433cd1f3b
Ramil Amerzyanov <ramilexe@gmail.com>
2021-05-19 16:36:32 +0300

    Docker CI/CD (#9)
    * CI/CD, configuration via ENV variables
    
    * fix pre-commit check
    
    * use setup-node version 2
    
    * fix GA: run on main branch
    
    * install yarn
    
    * fix path
    
    * fix github auth
    
    * docker build command fix

---
+++ newclient/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+JSON_STRING='window.configs = { \
+  "VUE_APP_HTTP_LINK":"'"${VUE_APP_HTTP_LINK}"'", \
+  "VUE_APP_WS_CLIENT":"'"${VUE_APP_WS_CLIENT}"'", \
+  "VUE_APP_OPENSEA_API":"'"${VUE_APP_OPENSEA_API}"'", \
+}'
+
+sed -i "s@// CONFIGURATIONS_PLACEHOLDER@${JSON_STRING}@" /usr/share/nginx/html/index.html
+exec "$@"
