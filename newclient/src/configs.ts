c269cc459d8a58fd44b155600e2b1054084c6b0b
ramil <ramilexe@gmail.com>
2021-05-19 17:29:51 +0300

    pass OPENSEA via env variable

--- newclient/src/configs.ts
+++ newclient/src/configs.ts
@@ -8,7 +8,7 @@ console.log(window.configs);
 const configs = {
     APOLLO_HTTP: window.configs && window.configs.VUE_APP_HTTP_LINK || process.env.VUE_APP_HTTP_LINK,
     APOLLO_WS: window.configs && window.configs.VUE_APP_WS_CLIENT || process.env.VUE_APP_WS_CLIENT,
-    OPENSEA: process.env.VUE_APP_OPENSEA_API,
+    OPENSEA: window.configs && window.configs.VUE_APP_OPENSEA_API || process.env.VUE_APP_OPENSEA_API,
     NODE_ENV: process.env.NODE_ENV,
     VERSION: process.env.VERSION,
     ROUTER_MODE: process.env.ROUTER_MODE || 'history'
